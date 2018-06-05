3d74762d3468b9ff7ac1924875121642f0736ec6
Eddie Harris and Hisato Kawaminami <hisato@>
2018-06-05 10:51:42 -0700

    add forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,26 @@
 $(document).ready(function() {
   $("#submit").click(function(){
     var sentence = $("input#sentence").val();
-    var splitted = sentence.split("");
+    var splitted = sentence.toLowerCase().split("");
+    var arr = [];
 
-
-    for (var i = 0; i < splitted.length; i += 1){
-      if (splitted[i] === "a" || splitted[i] === "e" || splitted[i] === "i" || splitted[i] === "o" || splitted[i] === "u") {
-        splitted[i] = "-";
+    splitted.forEach(function(split) {
+      if (split === "a" || split === "e" || split === "i" || split === "o" || split=== "u") {
+        arr.push(split = "-");
+      } else {
+        arr.push(split);
       }
-    }
+    })
+
+    // for (var i = 0; i < splitted.length; i += 1){
+    //   if (splitted[i] === "a" || splitted[i] === "e" || splitted[i] === "i" || splitted[i] === "o" || splitted[i] === "u") {
+    //     splitted[i] = "-";
+    //   }
+    // }
 
 
 
-    $("#formOne").hide();
-    $(".output").text(splitted);
+  //  $("#formOne").hide();
+    $(".output").text(arr);
   });
 });
