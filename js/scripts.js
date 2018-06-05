b68f82e97d9e2a2ce22ad198fecb4864cca02b34
Eddie Harris and Hisato Kawaminami <eddie@>
2018-06-05 11:35:22 -0700

    add webpage using checkbox

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,23 @@
-$(document).ready(function() {
-  $("#submit").click(function(){
-    var sentence = $("input#sentence").val();
-    var splitted = sentence.toLowerCase().split("");
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event){
+    event.preventDefault();
+    $("#work-responses").show();
     var arr = [];
 
-    splitted.forEach(function(split) {
-      if (split === "a" || split === "e" || split === "i" || split === "o" || split=== "u") {
-        arr.push(split = "-");
-      } else {
-        arr.push(split);
-      }
-    })
-
-    // for (var i = 0; i < splitted.length; i += 1){
-    //   if (splitted[i] === "a" || splitted[i] === "e" || splitted[i] === "i" || splitted[i] === "o" || splitted[i] === "u") {
-    //     splitted[i] = "-";
-    //   }
-    // }
+    $("input:radio[name=work-transportation]:checked").each(function(){
+      var workTransportationMode = $(this).val().toUpperCase();
+      arr.push(workTransportationMode);
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
 
 
+    $("#fun-responses").show();
+    $("input:checkbox[name=fun-transportation]:checked").each(function(){
+      var funTransportationMode = $(this).val();
+      $('#fun-responses').append(funTransportationMode + "<br>");
+    });
 
-  //  $("#formOne").hide();
-    $(".output").text(arr);
+    $('#test').append(arr + "<br>");
+    $('#transportation_survey').hide();
   });
 });
